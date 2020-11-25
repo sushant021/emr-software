562810d753a89d44969b7c7650fb036758e97916
sushant021 <nsushant021@gmail.com>
2020-11-25 16:33:20 -0500

    User groups created

--- emr/templatetags/custom_filters.py
+++ emr/templatetags/custom_filters.py
@@ -7,3 +7,8 @@ register = template.Library()
 def appointment_count(appointments, user):
     appointments = appointments.filter(user=user)
     return appointments.count()
+
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
